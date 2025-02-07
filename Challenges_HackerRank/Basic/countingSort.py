ecf0c32e5786ff1002f0563da0b30b18b6132a02
Lalset <tallesg.aragao@gmail.com>
2025-02-07 18:13:14 -0300

    Fixing code

--- Challenges_HackerRank/Basic/countingSort.py
+++ Challenges_HackerRank/Basic/countingSort.py
@@ -8,25 +8,11 @@ def countingSort(arr):
         counter[num] += 1
         
     return counter
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = countingSort(arr)
-
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
     
 # Local Mode:
+# if __name__ == '__main__':
+#     n = int(input("Enter how many numbers: "))
+#     arr = list(map(int, input("Enter numbers separated by space: ").split()))
 
-# n = int(input("Enter how many numbers: ").strip())
-# arr = list(map(int, input("Enter numbers separated by space: ").split()))
-
-# result = countingSort(arr)
-# print(" ".join(map(str, result)))
+#     result = countingSort(arr)
+#     print(" ".join(map(str, result)))
