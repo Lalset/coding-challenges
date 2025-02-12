53078894095c06be317d85874d90947da8da9997
Lalset <tallesg.aragao@gmail.com>
2025-02-12 10:33:28 -0300

    Adding new challenge

---
+++ Challenges_LeetCode/Easy/removeElement.py
@@ -0,0 +1,24 @@
+# LeetCode Mode:
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        
+        for j in range(len(nums)):
+            if nums [j] != val:
+                nums[i] = nums[j]
+                i += 1 
+                
+        return i
+    
+# Local Mode:
+
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
+val = int(input("Enter the value to remove: "))
+
+sol = Solution()
+r = sol.removeElement(nums, val)
+
+print(f"Number of remaining elements: {r}")
+print(f"Modified array: {nums[:r]}")
