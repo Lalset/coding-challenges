934857e58d5b8ba56ac5204a06be23a4d88c548c
Lalset <tallesg.aragao@gmail.com>
2025-02-14 12:52:46 -0300

    Adding new challenge

---
+++ Challenges_LeetCode/Easy/searchInsert.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
+    
+nums = list(map(int, input("Enter a list of numbers separated by space: ").split()))
+target = int(input("Enter the target number: "))
+
+sol = Solution()
+res = sol.searchInsert(nums, target)
+
+print(f"Index of target: {res}")
