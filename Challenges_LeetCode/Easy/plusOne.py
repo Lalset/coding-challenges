313ee1fa7042d3c1a609712ab83fd460b20a8dd9
Lalset <tallesg.aragao@gmail.com>
2025-02-17 22:38:37 -0300

    Adding a new challenge

---
+++ Challenges_LeetCode/Easy/plusOne.py
@@ -0,0 +1,24 @@
+# LeetCode Mode:
+
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+            
+        return [1] + digits
+  
+# Local Mode:
+  
+# digits = list(map(int, input("Enter the digits separated by space: ").split()))
+
+# sol = Solution()
+# result = sol.plusOne(digits)
+
+# print("Digits:", result)
