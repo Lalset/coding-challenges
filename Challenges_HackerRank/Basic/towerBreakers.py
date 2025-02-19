238eb8cfa50c4395d7f482f6f3856a5ef6a472ae
Lalset <tallesg.aragao@gmail.com>
2025-02-18 23:44:03 -0300

    Adding a new challenge

---
+++ Challenges_HackerRank/Basic/towerBreakers.py
@@ -0,0 +1,13 @@
+def towerBreakers(n, m):
+    if m == 1:
+        return 2
+    if n % 2 == 0:
+        return 2
+    return 1
+
+
+t = int(input("Enter the number of test cases: ").strip())
+
+for _ in range(t):
+    n, m = map(int, input("Enter the number of towers and the height of towers:").split())
+    print("Winner:", towerBreakers(n, m))
