19ac3630c157f5b19ebbfebd8aaab6a875993bcd
Lalset <tallesg.aragao@gmail.com>
2025-03-01 23:49:12 -0300

    Adding a new challenge

---
+++ Challenges_HackerRank/Basic/caesarCipher.py
@@ -0,0 +1,34 @@
+import os
+
+# LeetCode Mode:
+
+def caesarCipher(s, k):
+    encryp_text = ""
+
+    for char in s:
+        if char.isalpha():  
+            shift = k % 26  
+            if char.islower():
+                new_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))
+            else:
+                new_char = chr(((ord(char) - ord('A') + shift) % 26) + ord('A'))
+            encryp_text += new_char
+        else:
+            encryp_text += char  
+
+    return encryp_text
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
